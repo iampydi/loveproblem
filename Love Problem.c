79d57061653357aee4eea2ce9c51b843783c18c4
Pydi Harshvardhan <48127611+iampydi@users.noreply.github.com>
2019-03-01 19:20:09 +0530

    Add files via upload

---
+++ Love Problem.c
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+void Parents_Rejection();
+void Girl_Ditches();
+void Boy_Ditches();
+void Time_Pass();
+
+int main() 
+{
+int n;
+
+
+cout<<"Welcome To The Love Failure Program"<<endl;
+cout<<"\n-----------------------------------------\n"<<endl;
+cout<<"1) Parents Rejection"<<endl;
+cout<<"2) Girl Ditched"<<endl;
+cout<<"3) Boy Ditched"<<endl;
+cout<<"4) Time pass"<<endl;
+cout<<"Enter Your Situation = ";
+cin>>n;
+
+
+switch (n)
+{
+    case 1: 
+	Parents_Rejection();
+	break;
+    
+    case 2: 
+    Girl_Ditches();
+    break;
+    
+    case 3: 
+    Boy_Ditches();
+    break;
+    
+    case 4:
+    cout<<"\n----------------------\n"<<endl;
+    cout<<"Just Forget Everything And Move On";
+    break;
+    
+    default:
+    cout<<"Please Enter The Correct Situation By Choosing The Right Option";
+    
+}
+  return 0;
+}
+
+void Parents_Rejection()
+{
+	 cout<<"Here Is What You Can Do."<<endl;
+	 cout<<"\n------------------------------\n";
+    cout<<"1) Find the correct time when they are not tense or irritated.\n"<<endl;
+    cout<<"2)  Try to explain to your parents why you love your partner.List out the reasons why you and what made you fall for them in the first place, even though it may sound like a business proposal.\n"<<endl;
+    cout<<"3) Search for marriages in your family which are intercaste or unconventional and were successful.Use them as examples to prove your point.\n"<<endl;
+    cout<<"4)  Gain support.Get some people (like your married cousins) to support you when talking about your marriage.\n"<<endl;
+    cout<<"5)  Ask them to meet your future partner and then decide without any pressure.You should also ask them to not to be biased. \n"<<endl;
+    cout<<"6)  Try to keep your parents away from relatives or people who are against intercaste marriage. \n"<<endl;
+    cout<<"7) Tell them that marriage is all about a little bit of compromise and sacrifice, and that the two of you don't have any problem in doing the same.No marriage is without compromise.  \n"<<endl;
+    cout<<"8)  Never pressure them by saying that you will move out with your partner.This would create more hatred towards your relationship. \n  "<<endl;
+    cout<<"9)  Try to get at least one of your parents on your side.However, try not to cause a rift between them on this topic since they will attribute that to you and your partner. \n"<<endl;
+    cout<<"10) Do not beg or plead.Try doing it with dignity. And do not get into fights or yelling matches with them on this topic, since it will make you seem immature and unworthy of their consideration of your love.\n"<<endl;
+    cout<<"11) If nothing else works, tell them firmly and politely that this is the person you have chosen for yourself and they should try to accept it because at the end of the day it is your life that is at stake.If they don't respect your decision, don't let them drag you down! \n"<<endl;
+    cout<<"12) I HOPE THIS HELPED YOU"<<endl;
+}
+
+void Girl_Ditches()
+{
+	cout<<"Here Is What You Can Do"<<endl;
+	cout<<"\n------------------------------\n";
+    cout<<"If your love interest or crush passed you up for another guy, trust us, you are not alone. This happens to everybody, often more than once! Still hurts? That's okay. You are human, after all. Taking ownership of your feelings is the best way to move on. Keep reading for instructions on how to get over that girl, and fast.\n"<<endl;
+    cout<<"1) Take her off the pedestal. Best girl in the world? Never met, and never will meet another like her? It's common to feel this way, but trust us, it probably is not true. So let your friends talk trash about her. Remind yourself of the annoying things she did. Do what you need to do to remind yourself that nobody's perfect, especially her.\n "<<endl;
+    cout<<"2) Don't take it personally. Relationships end. It happens, and beating yourself up about it or trying to solve something about yourself will only make the heartache worse. Instead, try to accept that it was more about her than it was about you.\n "<<endl;
+    cout<<"3) Don't think about it. This can be a tough one. But dwelling on the ifs and buts and whys is helpful to no one, especially yourself. Find ways to get her out of your mind. \n"<<endl;
+    cout<<"4) Activate your life. Re-engage interests or people you missed out on while dating or pursuing Ms. Queen Bee. Join a sports team. Call up an old friend. Read a good book. Enjoy your own life and you will worry less about hers. After all, the best revenge is living well.\n "<<endl;
+    cout<<"5) Stay healthy. Hit the gym, go running, eat good foods. Keeping your body healthy is scientifically proven to help keep your mind happy, and that's really what this is all about. \n"<<endl;
+    cout<<"6) Don't worry about the other guy! You never know why people do the things they do. Obsessing over the guy she found will only make your own pain worse. \n"<<endl;
+    cout<<"7) Remember the other fish in the sea. Take some time and get your mind right, and then remind yourself that you will have plenty of other chances. Relationships are an adventure, and sometimes they do not end well. But there are plenty more of them out there, waiting to happen. Learn from them, and make them what you will.\n "<<endl;
+    cout<<"8) I HOPE THIS HELPED YOU"<<endl;
+}
+
+void Boy_Ditches()
+{
+	 cout<<"Here Is What You Can Do"<<endl;
+	 cout<<"\n------------------------------\n";
+    cout<<"1) Relationships can be amazing experiences, but with the high of a relationship, there is sometimes the low of a breakup. One of the worst kinds of breakups is where your partner leaves you for someone else. If you have been dating a guy who leaves you for another girl, you can reduce the length and intensity of your suffering by handling the breakup with dignity, taking care of your emotional state, and beginning to build a new life for yourself.\n"<<endl;
+    cout<<"2) Stay away from him. It's easy to keep the fantasy of some kind of relationship alive with the idea of staying friends. You may be able to be friends in the long run, but it's best to focus on yourself for a while and allow your feelings to die down before trying to have any kind of friendship with your ex. Trying to stay in contact can make the breakup worse if you aren't really over him or if he still has residual feelings for you.[1]It may also be really hard to see him with the new girl. Protect yourself from that emotional pain by keeping your distance, at least for a little while.Avoid interacting or seeing him on social media and avoid texting or calling. You may also want to get rid of anything that reminds you of him such as photos or gifts.\n"<<endl;
+    cout<<"3) Resist the urge to get back together. Breakups are painful but sometimes necessary. Trying to get back together will only extend the grieving process, and if he's with someone new, it'll likely just be you trying to repair the relationship, which puts you in a bad position where it's easy to continue getting hurt.[2] If he tries to keep something going while talking to the new girl, avoid this messy situation. Show him and everyone around you that you have the maturity to maintain a clean break, even if he doesn't.\n"<<endl;
+    cout<<"4) Speak positively about him. Try to take the higher road and avoid gossiping about what he did. You may feel like you need to vent, but this can create problems if you have mutual friends. Framing situations positively is better for your own mental health than venting in a negative way.[3]Try to understand his position. He may be doing what he thinks is best for his life. If he wasn't interested in trying to salvage your relationship, then either way, it's the best case scenario long-term for you to have broken up. If someone asks about the breakup, you can say It's been a difficult experience for me, but I wish him the best regardless.\n"<<endl;
+    cout<<"5) Respect his decision. You may not like it, and you don't have to, but try to understand that the decision has been made and the best thing for you to do is move on, as painful as that can be. Even if you invested a lot of time and energy into the relationship, if it's over then the best thing you can do is try to move forward.[4] You can say to him, I respect your decision even though I don't like it or agree with it.\n"<<endl;
+    cout<<"6) Keep her out of it. You may be tempted to attack the girl he leaves you for, but it's not her fault, and being rude to her will only make it harder for you to move on. Appreciate the positive side of another person being involved: it will make it easier for a clean break to happen, because you won't keep orbiting each other as each of you tries to cope with the pain of the situation.[5]If she tries to talk to you or gossips about you to other people, maintain your integrity by not responding to her bitterness. If you do want to respond to something she says, keep it simple and friendly. You can say It wasn't meant to be for us, but I hope it works out between the two of you. If she's a friend or if you have mutual friends, be extra cautious about not being resentful. You may want to be friends with her again in the future, or you may not want to poison your social circles by disrespecting her. If you need to vent, talk to a friend who isn't close to her (or the guy).\n"<<endl;
+    cout<<"7) I HOPE THIS HELPED YOU"<<endl;
+}
+
+  
